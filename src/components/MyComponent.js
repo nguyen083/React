9df1f6b284d2382bc2b90a8debca3c194d90ff95
nguyen083 <billbatri085@gmail.com>
2024-06-16 00:22:15 +0700

    cấu trúc của WEB React

---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>
+              My fist Components
+              {Math.random()}
+              </div>
+  
+        );
+    }
+}
+export default MyComponent
