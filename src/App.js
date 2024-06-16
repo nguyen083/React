f81e4696ad150084a5bb5bf40cd6ce70cb5da307
nguyen083 <billbatri085@gmail.com>
2024-06-16 13:58:04 +0700

    create state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import MyComponent from './components/MyComponent';
-function App() {
-  return (
-    <div className="App">
-      <MyComponent/>
-    </div>
-  );
-}
+import React from 'react';
 
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <MyComponent />
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,15 @@
 import React from "react";
 class MyComponent extends React.Component {
+    state = {
+        name: 'Nguyen',
+        address: "Ben Tre",
+        age: 26
+    };
     render() {
         return (
             <div>
-              My fist Components
-              </div>
-  
+              My name is {this.state.name} form {this.state.address}
+            </div>
         );
     }
 }
