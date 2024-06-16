fe872110401789f1a263c130d5240b911ade2661
nguyen083 <billbatri085@gmail.com>
2024-06-16 14:30:06 +0700

    event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,10 +5,20 @@ class MyComponent extends React.Component {
         address: "Ben Tre",
         age: 26
     };
+    handleClick = (event) => {
+        console.log("Click me button");
+        console.log(event);
+    }
+    handleOnMoveOver(event){
+        console.log(event);
+    }
+    //JSX
     render() {
         return (
             <div>
               My name is {this.state.name} form {this.state.address}
+              <br></br>
+            <button onClick={this.handleClick} on onMouseOver={this.handleOnMoveOver}>click me</button>
             </div>
         );
     }
