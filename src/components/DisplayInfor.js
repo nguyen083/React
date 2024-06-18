1b0ec755d33a7d65a126760be9600cef7db729dc
nguyen083 <billbatri085@gmail.com>
2024-06-19 01:37:00 +0700

    conditional output

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,19 +1,37 @@
 import React from 'react';
 class DisplayInfor extends React.Component {
+    state = {
+        check: true,
+    }
+    handleOnClick = (event) => {
+        this.setState({
+            check: !this.state.check,
+        });
+    }
     render() {
         const { listUders } = this.props;
         return (
             <div>
+                <span onClick={(event) => { this.handleOnClick(event) }} style={{color: 'blue',textDecoration: 'underline'}}>{this.state.check? 'Hide': 'Show'}</span>
                 {
-                    listUders.map((item) => {
-                        return (
-                            <div key={item.id}>
-                                <p>My name is {item.name} and {item.age}</p>
-                            </div>
-                        );
-                    })
+                    this.state.check && <div>
+                        {
+                            listUders.map((item) => {
+                                return (<div key={item.id} class= {item.age >= 26 ? 'red': 'green'}>My name is {item.name} and {item.age} <hr /></div>)
+                            })
+                        }
+                    </div>
                 }
-                {/* My name is {name} and {age} */}
+                {/* {
+                    listUders.map((item) => {
+                        // return item.age >= 26 ? (
+                        //     <div key={item.id} class="red">My name is {item.name} and {item.age} <hr /></div>
+                        // ) : (<div key={item.id} class="green">My name is {item.name} and {item.age}<hr /></div>);
+                        return (<div key={item.id} class= {item.age >= 26 ? 'red': 'green'}>My name is {item.name} and {item.age} <hr /></div>)
+                            
+                        
+                    })
+                } */}
             </div>
         );
     }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,11 +5,11 @@ class MyComponent extends React.Component {
 
     state = {
         listUders: [
-            { id: 1, name: 'Nguyên', age: 22 },
-            { id: 2, name: 'Phong', age: 22 },
-            { id: 3, name: 'Trường', age: 22 },
-            { id: 4, name: 'Duy', age: 22 },
-            { id: 5, name: 'Phú', age: 22 },
+            { id: 1, name: 'Nguyên', age: 26 },
+            { id: 2, name: 'Phong', age: 27 },
+            { id: 3, name: 'Trường', age: 25 },
+            { id: 4, name: 'Duy', age: 20 },
+            { id: 5, name: 'Phú', age: 28 },
         ],
     }
     //JSX
