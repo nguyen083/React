b814d0af1c62b3527a8eb9ade3080a23e4580384
nguyen083 <billbatri085@gmail.com>
2024-06-18 18:18:37 +0700

    setstate for form

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,24 +1,36 @@
 import React from "react";
 class MyComponent extends React.Component {
     state = {
-        name: 'Nguyen',
-        address: "Ben Tre",
-        age: 26
+        name: 'Nguyên',
+        address: "Bến Tre",
+        age: '22',
     };
-    handleClick(event) {
+    handleClick = (event) => {
         console.log("Click me button");
         this.setState({
             name: "Nguyễn Huỳnh Nguyên",
-            age: Math.floor((Math.random()*100)+1)
+            age: Math.floor((Math.random() * 100) + 1)
         })
+    }
+    handleOnChange = (event) => {
+        this.setState({
+            name: event.target.value,
+        });
+    }
+    handleonSubmit = (event) => {
+        event.preventDefault();// ngăn chặn sự kiện mặc định load lại trang
+        console.log(this.state);
     }
     //JSX
     render() {
         return (
             <div>
-              My name is {this.state.name} and I'm {this.state.age}
-              <br></br>
-            <button onClick={(event) => {this.handleClick(event)}} >click me</button>
+                My name is {this.state.name} and I'm {this.state.age}
+                <br></br>
+                <form onSubmit={(event => { this.handleonSubmit(event) })}>
+                    <input type="text" value={this.state.name} onChange={(event) => { this.handleOnChange(event) }} />
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         );
     }
