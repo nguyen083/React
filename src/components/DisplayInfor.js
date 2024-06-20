a740e33a62c2c93b79e88a7bdd27b8ffe317c345
nguyen083 <billbatri085@gmail.com>
2024-06-20 22:28:52 +0700

    function component

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,60 +1,55 @@
 import React from 'react';
 import './DisplayInfor.scss'
-class DisplayInfor extends React.Component {
-    constructor(props) {
-        console.log('constructor: 1');
-        super(props);
-        this.state = {
-            check: true,
-        }
-    }
+// class DisplayInfor extends React.Component {
 
-    componentDidMount() {
 
-        console.log('componentDidMount');
-        setTimeout(() => {
-            document.title = "DisplayInfor"
-        }, 3000)
-    }
-    componentDidUpdate(prevProps, prevState, snapshot){
-        console.log('componentDidUpdate');
-    }
-    handleOnClick = (event) => {
-        this.setState({
-            check: !this.state.check,
-        });
-    }
-    render() {
-        console.log('render');
-        const { listUders } = this.props;
+//     render() {
+//         console.log('render');
+//         const { listUders } = this.props;
+//         return (
+//             <div className='display-infor-container'>
+//                 {
+//                     true && <>
+//                         {
+//                             listUders.map((item) => {
+//                                 return (<><div key={item.id} className={item.age >= 26 ? 'red' : 'green'}>My name is {item.name} and {item.age} </div>
+//                                     <button className='btnDel' onClick={() => this.props.handleDeleteUser(item.id)}>Delete</button>
+//                                     <hr /></>)
+//                             })
+//                         }
+
+//                     </>
+//                 }
+//                 {/* {
+//                     listUders.map((item) => {
+//                         // return item.age >= 26 ? (
+//                         //     <div key={item.id} class="red">My name is {item.name} and {item.age} <hr /></div>
+//                         // ) : (<div key={item.id} class="green">My name is {item.name} and {item.age}<hr /></div>);
+//                         return (<div key={item.id} class= {item.age >= 26 ? 'red': 'green'}>My name is {item.name} and {item.age} <hr /></div>)
+                            
+                        
+//                     })
+//                 } */}
+//             </div>
+//         );
+//     }
+// }
+const DisplayInfor = (props) => {
+            const { listUders } = props;
         return (
             <div className='display-infor-container'>
-                {/* <img src={logo} alt="logo" /> */}
-                <span onClick={(event) => { this.handleOnClick(event) }} style={{ color: 'blue', textDecoration: 'underline' }}>{this.state.check ? 'Hide' : 'Show'}</span>
                 {
-                    this.state.check && <>
+                    true && <>
                         {
                             listUders.map((item) => {
                                 return (<><div key={item.id} className={item.age >= 26 ? 'red' : 'green'}>My name is {item.name} and {item.age} </div>
-                                    <button className='btnDel' onClick={() => this.props.handleDeleteUser(item.id)}>Delete</button>
+                                    <button className='btnDel' onClick={() => props.handleDeleteUser(item.id)}>Delete</button>
                                     <hr /></>)
                             })
                         }
-
                     </>
                 }
-                {/* {
-                    listUders.map((item) => {
-                        // return item.age >= 26 ? (
-                        //     <div key={item.id} class="red">My name is {item.name} and {item.age} <hr /></div>
-                        // ) : (<div key={item.id} class="green">My name is {item.name} and {item.age}<hr /></div>);
-                        return (<div key={item.id} class= {item.age >= 26 ? 'red': 'green'}>My name is {item.name} and {item.age} <hr /></div>)
-                            
-                        
-                    })
-                } */}
             </div>
         );
-    }
 }
 export default DisplayInfor;
