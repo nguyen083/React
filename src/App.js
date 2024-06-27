50eb0d3770e0fb505f49644ed123101f1a68bb4e
nguyen083 <billbatri085@gmail.com>
2024-06-27 22:08:06 +0700

    set up project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.scss';
-import MyComponent from './components/MyComponent';
-import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='app-container'>
-        <MyComponent />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className='app-container'>
+      hello world!
+    </div>
+  );
 }
 export default App;
